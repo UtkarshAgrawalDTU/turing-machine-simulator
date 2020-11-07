4c1daa651ec0ffa166799e74a5365d7c29e62420
Utkarsh Agrawal <agrawal.utkarsh8@gmail.com>
2020-11-07 16:50:35 +0530

    div3 working and added buttons

--- src/App.js
+++ src/App.js
@@ -17,19 +17,12 @@ class App extends Component
   {
     this.setState({
       [event.target.id] : event.target.value,
-      pointer : 0,
+      pointer : 1,
       currentState : 'start',
+      input : 'B' + event.target.value + 'B',
     })
   }
 
-  start()
-  {
-    this.setState(prevState => ({
-      input : prevState.input + " ",
-    }))
-    console.log(this.state)
-    this.trigger();
-  }
 
   trigger()
   {   
@@ -37,29 +30,190 @@ class App extends Component
     if(this.state.currentState !== 'accept' && this.state.currentState !== 'reject')
     {
       if(this.state.simulator === "Palindrome")
-        	this.simulatePalindrome();
+          this.simulatePalindrome();
+          
+      else if(this.state.simulator === "Divisible by 3")
+          this.simulateDiv3();
+
+      else if(this.state.simulator === "3 Equal Lengths")
+          this.simulate3Equal();
     }
       
   }
+
+  simulate3Equal()
+  {
+
+  }
+
+
+  simulateDiv3()
+  {
+      var currState = this.state.currentState;
+      var pointer = this.state.pointer;
+      var input = this.state.input;
+
+      var newinput = input;
+      var newpointer = pointer;
+      var newState = currState;
+    
+      if(currState === 'start')
+      {
+        if(input[pointer] === '1' || input[pointer] === '4' || input[pointer] === '7')
+        {
+          newpointer += 1;
+          newState = 'q1';
+        }
+
+        else if(input[pointer] === '2' || input[pointer] === '5' || input[pointer] === '8')
+        {
+          newpointer += 1;
+          newState = 'q2';
+        }
+
+        else if(input[pointer] === '0' || input[pointer] === '3' || input[pointer] === '6' || input[pointer] === '9')
+        {
+          newpointer += 1;
+          newState = 'q0';
+        }
+
+        else if(input[pointer] === 'B')
+        {
+          newpointer -= 1;
+          newState = 'reject';
+        }
+
+        else
+          newState = 'reject';
+      }
+
+      else if(currState === 'q0')
+      {
+        if(input[pointer] === '1' || input[pointer] === '4' || input[pointer] === '7')
+        {
+          newpointer += 1;
+          newState = 'q1';
+        }
+
+        else if(input[pointer] === '2' || input[pointer] === '5' || input[pointer] === '8')
+        {
+          newpointer += 1;
+          newState = 'q2';
+        }
+
+        else if(input[pointer] === '0' || input[pointer] === '3' || input[pointer] === '6' || input[pointer] === '9')
+        {
+          newpointer += 1;
+          newState = 'q0';
+        }
+
+        else if(input[pointer] === 'B')
+        {
+          newpointer -= 1;
+          newState = 'accept';
+        }
+
+        else
+          newState = 'reject';
+      }
+
+
+      else if(currState === 'q1')
+      {
+        if(input[pointer] === '1' || input[pointer] === '4' || input[pointer] === '7')
+        {
+          newpointer += 1;
+          newState = 'q2';
+        }
+
+        else if(input[pointer] === '2' || input[pointer] === '5' || input[pointer] === '8')
+        {
+          newpointer += 1;
+          newState = 'q0';
+        }
+
+        else if(input[pointer] === '0' || input[pointer] === '3' || input[pointer] === '6' || input[pointer] === '9')
+        {
+          newpointer += 1;
+          newState = 'q1';
+        }
+
+        else if(input[pointer] === 'B')
+        {
+          newpointer -= 1;
+          newState = 'reject';
+        }
+
+        else
+          newState = 'reject';
+      }
+
+      else if(currState === 'q2')
+      {
+        if(input[pointer] === '1' || input[pointer] === '4' || input[pointer] === '7')
+        {
+          newpointer += 1;
+          newState = 'q0';
+        }
+
+        else if(input[pointer] === '2' || input[pointer] === '5' || input[pointer] === '8')
+        {
+          newpointer += 1;
+          newState = 'q1';
+        }
+
+        else if(input[pointer] === '0' || input[pointer] === '3' || input[pointer] === '6' || input[pointer] === '9')
+        {
+          newpointer += 1;
+          newState = 'q2';
+        }
+
+        else if(input[pointer] === 'B')
+        {
+          newpointer -= 1;
+          newState = 'reject';
+        }
+
+        else
+          newState = 'reject';
+      }
+
+      else
+        newState = 'reject';
+
+      setTimeout(() => {
+        this.setState({
+          pointer : newpointer,
+          input : newinput,
+          currentState : newState,
+        })
+        this.trigger();
+      },  1000);
+        
+  }
+
+
+
+
 
   simulatePalindrome()
   {
     const stateList = {
       start : {
         a : {
-          write : ' ',
+          write : 'B',
           move : 'R',
           newState : 'haveA'
         },
 
         b : {
-          write : '',
+          write : 'B',
           move : 'R',
           newState : 'haveB'
         },
 
-        blank : {
-          write : ' ',
+        B : {
+          write : 'B',
           move : 'R',
           newState : 'accept'
         }
@@ -78,8 +232,8 @@ class App extends Component
           newState : 'haveA'
         },
 
-        blank : {
-          write : ' ',
+        B : {
+          write : 'B',
           move : 'L',
           newState : 'matchA'
         }
@@ -98,8 +252,8 @@ class App extends Component
           newState : 'haveB'
         },
 
-        blank : {
-          write : ' ',
+        B : {
+          write : 'B',
           move : 'L',
           newState : 'matchB'
         }
@@ -107,7 +261,7 @@ class App extends Component
 
       matchA : {
         a : {
-          write : ' ',
+          write : 'B',
           move : 'L',
           newState : 'back'
         },
@@ -118,8 +272,8 @@ class App extends Component
           newState : 'reject'
         },
 
-        blank : {
-          write : ' ',
+        B : {
+          write : 'B',
           move : 'L',
           newState : 'accept'
         }
@@ -133,13 +287,13 @@ class App extends Component
         },
 
         b : {
-          write : ' ',
+          write : 'B',
           move : 'L',
           newState : 'back'
         },
 
-        blank : {
-          write : ' ',
+        B : {
+          write : 'B',
           move : 'L',
           newState : 'accept'
         }
@@ -158,8 +312,8 @@ class App extends Component
           newState : 'back'
         },
 
-        blank : {
-          write : ' ',
+        B : {
+          write : 'B',
           move : 'R',
           newState : 'start'
         }
@@ -171,8 +325,6 @@ class App extends Component
       reject : {
       }
     };
-    
-    console.log(this.state)
 
       var currState = this.state.currentState;
       var pointer = this.state.pointer;
@@ -186,7 +338,7 @@ class App extends Component
       if(input[pointer] === 'a')
       {
         newState = conditions['a'].newState;
-        newinput = input.substring(0, pointer) + conditions['a'].write + input.substring(pointer + 1) + " ";
+        newinput = input.substring(0, pointer) + conditions['a'].write + input.substring(pointer + 1);
         if(conditions['a'].move === 'L')
           newpointer -=1;
         else
@@ -196,18 +348,18 @@ class App extends Component
       else if(input[pointer] === 'b')
       {
         newState = conditions['b'].newState;
-        newinput = input.substring(0, pointer) + conditions['b'].write + input.substring(pointer + 1) + " ";
+        newinput = input.substring(0, pointer) + conditions['b'].write + input.substring(pointer + 1);
         if(conditions['b'].move === 'L')
           newpointer -=1;
         else
           newpointer +=1;
       }
 
-      else if(input[pointer] === ' ')
+      else if(input[pointer] === 'B')
       {
-        newState = conditions['blank'].newState;
-        newinput = input.substring(0, pointer) + conditions['blank'].write + input.substring(pointer + 1) + " ";
-        if(conditions['blank'].move === 'L')
+        newState = conditions['B'].newState;
+        newinput = input.substring(0, pointer) + conditions['B'].write + input.substring(pointer + 1);
+        if(conditions['B'].move === 'L')
           newpointer -=1;
         else
           newpointer +=1;
@@ -222,21 +374,37 @@ class App extends Component
           currentState : newState,
         })
         this.trigger();
-      },  1000);
+      },  750);
       
   }
 
   selectPalindrome()
   {
     this.setState({
-      simulator : "Palindrome"
+      simulator : "Palindrome",
+      pointer : 1,
+      currentState : "start", 
+      input : 'B' + this.state.input_user + 'B',
     })
   }
 
   selectDiv3()
   {
     this.setState({
-      simulator : "Divisible by 3"
+      simulator : "Divisible by 3",
+      pointer : 1,
+      currentState : "start", 
+      input : 'B' + this.state.input_user + 'B',
+    })
+  }
+
+  select3Equal()
+  {
+    this.setState({
+      simulator : "3 Equal Lengths",
+      pointer : 1,
+      currentState : "start", 
+      input : 'B' + this.state.input_user + 'B',
     })
   }
 
@@ -247,25 +415,33 @@ class App extends Component
     super();
     this.state = {
       loading : true,
-      input : "",
-      pointer : 0,      
+      input : "BB",
+      input_user : "",
+      pointer : 1,      
       currentState : "start",
       simulator : "",
     }
 
     this.handleChange = this.handleChange.bind(this)
     this.simulatePalindrome = this.simulatePalindrome.bind(this)
+    this.simulateDiv3 = this.simulateDiv3.bind(this)
+    this.simulate3Equal = this.simulate3Equal.bind(this)
     this.trigger = this.trigger.bind(this)
     this.selectPalindrome = this.selectPalindrome.bind(this)
     this.selectDiv3 = this.selectDiv3.bind(this)
-    this.start = this.start.bind(this)
+    this.select3Equal = this.select3Equal.bind(this)
   }
 
-  render(){
-    console.log(this.state)
+
+
+  render()
+  {
+
     const accept = this.state.currentState === 'accept';
     const reject = this.state.currentState === 'reject'; 
     const simulator = this.state.simulator === "" ? "Select Problem" : this.state.simulator;
+
+
     return (
       <div className="App">
         <div className = "container">
@@ -275,26 +451,52 @@ class App extends Component
 
           <div className="input-group mb-3">
             <div className="input-group-prepend">
-    <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{simulator}</button>
+              <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{simulator}</button>
               <div className="dropdown-menu">
                 <a className="dropdown-item" onClick = {this.selectPalindrome}>Palindrome</a>
                 <a className="dropdown-item" onClick = {this.selectDiv3} >Divisible by 3</a>
-                <a className="dropdown-item">Other</a>
+                <a className="dropdown-item" onClick = {this.select3Equal}>Three Equal Lengths</a>
               </div>
             </div>
-            <input id = "input" value = {this.state.input} onChange = {this.handleChange} className="form-control form-control-lg" type="text" placeholder="Enter string of a and b here" />
+            <input id = "input_user" value = {this.state.input_user} onChange = {this.handleChange} className="form-control form-control-lg" type="text" placeholder="Enter string of a and b here" />
           </div>
           
           
-          <br />
-          <br />
-          <br />
-
+        <div className = "row my-5">
           <Tape input = {this.state.input} pointer = {this.state.pointer}/>
-          <br />
-          <br />
-          <br />
-          <button type="button" class="btn btn-primary" onClick = {this.start}>Simulate</button>
+        </div>
+          
+
+        <div id="controls-container" class="row text-center btn-group">
+            <div class="col-xs-1 mx-1">
+              <button type="button" class="btn btn-warning btn-xs text-center">
+              <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-arrow-counterclockwise" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"/>
+                <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z"/>
+              </svg>
+              <br />
+                Reset
+              </button>
+            </div>
+            <div class="col-xs-2 col-xs-offset-4 text-center mx-1">
+              <button type="button" class="btn btn-primary text-center">
+              <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-skip-end-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M12 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5z"/>
+                <path d="M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
+              </svg>
+              <br />
+                Step
+              </button>
+            </div>
+            <div class="col-xs-1 mx-1">
+              <button type="button" onClick = {this.trigger} class="btn btn-light text-center tm-btn-diagram tm-run">
+              <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-play" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M10.804 8L5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z"/>
+              </svg>
+              <br />Run</button>
+            </div>
+        </div>
+
 
           {accept ? 
           <div className="alert alert-success my-3" role="alert">
@@ -306,7 +508,7 @@ class App extends Component
           <div className="alert alert-danger my-4" role="alert">
             Failure!
           </div> : <div /> 
-          }
+          }          
           
         </div>
       </div>
